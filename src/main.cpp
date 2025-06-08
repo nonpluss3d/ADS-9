aff2e2be58582bcdc8e4129c17b8e2c1a87abe2c
nonpluss3d <al.kistanoff2016@yandex.ru>
2025-06-08 18:02:28 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,22 @@
 // Copyright 2022 NNTU-CS
 #include <chrono>
+#include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <random>
+#include <utility>
 #include <vector>
 
 #include "tree.h"
 
 template <typename Func, typename... Args>
-long long measureTime(Func func, Args&&... args) {
+int64_t measureTime(Func func, Args&&... args) {
   auto start = std::chrono::high_resolution_clock::now();
   func(std::forward<Args>(args)...);
   auto end = std::chrono::high_resolution_clock::now();
-  return std::chrono::duration_cast<std::chrono::microseconds>(end - start)
-      .count();
+  return static_cast<int64_t>(
+      std::chrono::duration_cast<std::chrono::microseconds>(end - start)
+          .count());
 }
 
 int main() {
@@ -33,9 +36,9 @@ int main() {
     int total = tree.getTotalPermutations();
     int randomNum = dist(generator) % total + 1;
 
-    long long timeGetAllPerms = measureTime([&]() { getAllPerms(tree); });
-    long long timeGetPerm1 = measureTime([&]() { getPerm1(tree, randomNum); });
-    long long timeGetPerm2 = measureTime([&]() { getPerm2(tree, randomNum); });
+    int64_t timeGetAllPerms = measureTime([&]() { getAllPerms(tree); });
+    int64_t timeGetPerm1 = measureTime([&]() { getPerm1(tree, randomNum); });
+    int64_t timeGetPerm2 = measureTime([&]() { getPerm2(tree, randomNum); });
 
     dataFile << n << "," << timeGetAllPerms << "," << timeGetPerm1 << ","
              << timeGetPerm2 << "\n";
